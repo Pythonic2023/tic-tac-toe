0c9277a966dac87f6a2a98094d20248538c17f2f
Pythonic <124443031+Pythonic2023@users.noreply.github.com>
2026-07-19 13:04:43 -0400

    Save changes for player choice in gameControl.

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ let gameBoard = (function(){
     let count = 1;
 
     for(let i = 0; i < 9; i+=3){
-        gameBoardArray.push({row: count, sectionA: "a", sectionB: "b", sectionC: "c"});
+        gameBoardArray.push({row: count, a:"", b:"", c: ""});
         count += 1;
     }
 
@@ -55,3 +55,27 @@ let playerTwo = playerFactory("ybboB");
         // listen for user input
         // fill array with user input
         // update if either row is full of x or o and continue if not. if all spots are filled and neither contain full row make a tie.
+let startGame = (function(){
+    console.log("Welcome to Tic Tac Toe!")
+    console.log(`${playerOne.getPlayerName()}`);
+    console.log(`${playerTwo.getPlayerName()}`);
+    // Retreive player moves
+
+    let playerOneMove = playerOne.playerMove().split("");
+    let rowPick = playerOneMove[0];
+    let cellPick = playerOneMove[1];
+
+    let row = gameBoard.gameBoardArray[rowPick];
+    console.log(row);
+    
+    let keys = Object.keys(row);
+    keys.forEach(key => {
+        console.log(key === key);
+    });
+    
+    // Apply their choices to the gameBoard object, only if someone has not picked the spot already.
+
+    // HOW I CAN ACCESS THE OBJECTS ARRAY AND PROPERTIES
+    //console.log(gameBoard.gameBoardArray[1].sectionA);
+
+}());
